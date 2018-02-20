bdf7c827d926c058abb3625557be910241c716b0
ArtemeeSenin <artemee.senin@gmail.com>
2018-02-21 01:54:12 +0300

    created local component

---
+++ src/components/Info.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="info">
+    <p>Vue.js is great!</p>
+    <hr>
+  </div>
+</template>
+
+<style>
+  hr {
+    border: 1px solid darkseagreen;
+  }
+</style>
+
--- src/components/User.vue
+++ src/components/User.vue
@@ -2,15 +2,21 @@
   <div id="user">
     <h3>Userpage</h3>
     <p>My username is {{username}}</p>
+    <app-info></app-info>
   </div>
 </template>
 
 <script>
+import Info from './Info.vue';
+
 export default {
   data(){
     return {
       username: 'Artemee'
     }
+  },
+  components: {
+    'app-info': Info
   }
 }
 </script>
