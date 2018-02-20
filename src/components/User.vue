be0ab714ce50a317b6900030129fdf9b90dcb633
ArtemeeSenin <artemee.senin@gmail.com>
2018-02-21 01:39:06 +0300

    User component

---
+++ src/components/User.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="user">
+    <h3>Userpage</h3>
+    <p>My username is {{username}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      username: 'Artemee'
+    }
+  }
+}
+</script>
+
+<style>
+  p {
+    color: cornflowerblue;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
